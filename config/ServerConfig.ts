c45854ec2a08f40504b3f6346eb797b9351ff7d8
jkkang <jkkang@smartauth.kr>
2021-07-12 11:42:27 +0900

    config 설정 추가

---
+++ config/ServerConfig.ts
@@ -0,0 +1,9 @@
+export const httpLinkURI = process.env.SERVER_URL;
+export const webSocketURI = process.env.WEBSOCKET_URL;
+export const feedSocketURI = process.env.FEEDSOCKET_URL;
+
+export const walletFeeURL = process.env.WALLET_FEE_URL;
+export const walletDataURL = process.env.WALLET_DATA_URL;
+export const walletVoteURL = process.env.WALLET_VOTE_URL;
+
+export const voteResultURL = process.env.VOTE_RESULT_URL;
---
+++ config/keys.ts
@@ -0,0 +1,5 @@
+export const LOCALSTORAGE_KEY = 'VoteraLocalData';
+export const LOCALSTORAGE_SERVICE = 'voteralocal';
+
+export const AUTHCONTEXT_AUTH_COOKIE = 'authcookie';
+export const LOCAL_TEMP_PROPOSALS = 'TEMP_PROPOSALS';
